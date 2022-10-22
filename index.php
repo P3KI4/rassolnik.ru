8d38cccfa6bbd1f65e77e5f098518784e272fa9f
P3KI4 <polin.kostya@bk.ru>
2022-10-22 13:11:57 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<title>rassolnik.ru</title>
+</head>
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="content">
+				<nav class="header__nav">
+					<ul class="nav__links">
+						<li class="header__icon"><div>	</div></li>
+						<li><a href="auctions.php" class="link">Аукционы</a></li>
+						<li><a href="members.php" class="link">Участники</a></li>
+						<li><a href="gallery.php" class="link">Галлерея</a></li>
+						<li><a href="music.php" class="link">Музыка</a></li>
+						<li><a href="photo.php" class="link">Фото</a></li>
+						<li><a href="craft.php" class="link">Ремесло</a></li>
+						<li><a href="design.php" class="link">Дизайн</a></li>
+						<li><a href="pictures.php" class="link">Арт</a></li>
+						<li><a href="poetry.php" class="link">Поэзия</a></li>
+						<li><a href="scene.php" class="link">Сцена</a></li>
+						<li><a href="humor.php" class="link">Юмор</a></li>
+						<li><a href="beauty.php" class="link">Красота</a></li>
+						<li class="header__search"><div>	</div></li>
+						<li class="header__signup"><div>	</div></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<main class="main">
+			<div class="content">
+				<div class="main__discription">
+					<div class="main__headtxt">Аукционы</div>
+					<div class="main__undertext">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam praesentium unde sit mollitia qui iusto possimus laborum repellendus magni, similique eveniet tempora voluptas fugiat, voluptates ex dolores, error ipsum ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium itaque sapiente pariatur, modi eaque ex rerum veritatis possimus? Ab omnis autem quidem harum ipsum expedita nostrum. Quaerat deleniti incidunt quos?</div>
+					
+				</div>
+			</div>
+			<div class="content">
+				<div class="main__grid">
+				<div class="main__pictures">
+					<section class="main__gridpictures">
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+						<div class="picture__item picture"></div>
+					</section>
+				</div>
+			</div>
+			<div class="main__button">
+				<div class="content">
+					<input type="button" value="Подать заявку на аукцион">
+				</div>
+			</div>
+		</main>
+		<footer class="footer">
+
+		</footer>
+	</div>
+</body>
+</html>
