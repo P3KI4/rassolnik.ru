c3b1ed091f462719af15aaddb07ed7ed42da00c3
P3KI4 <polin.kostya@bk.ru>
2022-10-23 19:29:47 +0500

    upd
    Выпадающие списки

--- index.php
+++ index.php
@@ -14,18 +14,67 @@
 				<nav class="header__nav">
 					<ul class="nav__links">
 						<li class="header__icon"><div>	</div></li>
-						<li><a href="auctions.php" class="link">Аукционы</a></li>
-						<li><a href="members.php" class="link">Участники</a></li>
-						<li><a href="gallery.php" class="link">Галлерея</a></li>
-						<li><a href="music.php" class="link">Музыка</a></li>
-						<li><a href="photo.php" class="link">Фото</a></li>
-						<li><a href="craft.php" class="link">Ремесло</a></li>
-						<li><a href="design.php" class="link">Дизайн</a></li>
-						<li><a href="pictures.php" class="link">Арт</a></li>
-						<li><a href="poetry.php" class="link">Поэзия</a></li>
-						<li><a href="scene.php" class="link">Сцена</a></li>
-						<li><a href="humor.php" class="link">Юмор</a></li>
-						<li><a href="beauty.php" class="link">Красота</a></li>
+						<li><a href="auctions.php" class="links__static headers">Аукционы</a></li>
+						<li><a href="members.php" class="links__static headers">Участники</a></li>
+						<li><a href="gallery.php" class="links__static headers">Галлерея</a></li>
+						
+						<li><a href="#" class="links__drop headers">Музыка</a>
+								<ul>
+									<li><a href="#">Поп</a></li>
+									<li><a href="#">Рок</a></li>
+									<li><a href="#">Реп</a></li>
+									<li><a href="#">Джаз</a></li>
+								</ul>
+						</li>
+
+						<li><a href="photo.php" class="links__drop headers">Фото</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="craft.php" class="links__drop headers">Ремесло</a>
+								<ul>
+									<li><a href="#">Гончарное</a></li>
+									<li><a href="#">Кузнечное</a></li>
+								</ul>
+						</li>
+						<li><a href="design.php" class="links__drop headers">Дизайн</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="pictures.php" class="links__drop headers">Арт</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="poetry.php" class="links__drop headers">Поэзия</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="scene.php" class="links__drop headers">Сцена</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="humor.php" class="links__drop headers">Юмор</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
+						<li><a href="beauty.php" class="links__drop headers">Красота</a>
+								<ul>
+									<li><a href="#">Еда</a></li>
+									<li><a href="#">Страны</a></li>
+								</ul>
+						</li>
 						<li class="header__search"><div>	<img src="image/search.png" alt="" srcset="">	</div></li>
 						<li class="header__signup"><div>	<img src="image/user.png" alt="" srcset=""> </div></li>
 					</ul>
@@ -34,11 +83,10 @@
 		</header>
 		<main class="container">
 				<div class="content">
-				<div class="main__discription">
-					<div class="main__headtxt">Аукционы</div>
-					<div class="main__undertext">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam praesentium unde sit mollitia qui iusto possimus laborum repellendus magni, similique eveniet tempora voluptas fugiat, voluptates ex dolores, error ipsum ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium itaque sapiente pariatur, modi eaque ex rerum veritatis possimus? Ab omnis autem quidem harum ipsum expedita nostrum. Quaerat deleniti incidunt quos?</div>
-					
-				</div>
+					<div class="main__discription">
+						<div class="main__headtxt">Аукционы</div>
+						<div class="main__undertext">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam praesentium unde sit mollitia qui iusto possimus laborum repellendus magni, similique eveniet tempora voluptas fugiat, voluptates ex dolores, error ipsum ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium itaque sapiente pariatur, modi eaque ex rerum veritatis possimus? Ab omnis autem quidem harum ipsum expedita nostrum. Quaerat deleniti incidunt quos?</div>
+					</div>
 				</div>
 				<div class="content">
 				<div class="main__grid">
